2638c2f9da43f05d9cc0ec2b7e1363de19e46ca2
yichong <yichong.joe@gmail.com>
2018-11-07 12:22:51 +0800

    modify TopicWeightAppender;

--- mm-basecode/src/main/java/yich/base/topic/TopicWeightAppender.java
+++ mm-basecode/src/main/java/yich/base/topic/TopicWeightAppender.java
@@ -1,16 +1,18 @@
 package yich.base.topic;
 
 
+import yich.base.dbc.Require;
 import yich.base.logging.JUL;
 import yich.base.predicate.AbsPredicate;
 import yich.base.predicate.WeightAppender;
 
+import java.util.function.Predicate;
 import java.util.logging.Logger;
 
 public class TopicWeightAppender extends WeightAppender<Topic> {
     private static Logger logger = JUL.getLogger(TopicWeightAppender.class);
 
-    private boolean overwrite = false;
+    private Predicate<Topic> overwrite = null;
 
     public TopicWeightAppender(String name, AbsPredicate<Topic> filter) {
         super(name, filter);
@@ -26,21 +28,36 @@ public class TopicWeightAppender extends WeightAppender<Topic> {
         return this;
     }
 
-    public boolean isOverwrite() {
-        return overwrite;
+    public boolean isOverwrite(Topic topic) {
+        return overwrite == null ? false : overwrite.test(topic);
     }
 
     public TopicWeightAppender setOverwrite(boolean overwrite) {
-        this.overwrite = overwrite;
+        this.overwrite = topic -> true;
+        return this;
+    }
+
+    public TopicWeightAppender setOverwrite(Predicate<Topic> predicate) {
+        Require.argumentNotNull(predicate);
+        this.overwrite = predicate;
         return this;
     }
 
     @Override
     protected void addWeight(Topic topic, int weight) {
-        if (topic != null && (overwrite || topic.getWeight() < weight)) {
+        if (topic == null) {
+            logger.warning("** Topic object is null...");
+            return;
+        }
+        if (isOverwrite(topic) || topic.getWeight() < weight) {
             topic.setWeight(weight);
             logger.info("** '" + topic.getUrl() + "' set weight to '" + weight + "'");
+        } else {
+            logger.info("** '" + topic.getUrl() + "'(weight=" + topic.getWeight() +
+                    ") failed to set weight to '" + weight + "'(original weight cannot be overwritten).");
         }
     }
+
+
 
 }
